891ba19a2fa3f321c2cb26a8e6e93000c4a49646
Evan <116227653+itsevanb@users.noreply.github.com>
2023-06-19 14:44:33 -0500

    initial layout build
    JSON file for storage 'list of dictionaries for now'
    html templating using flask
    CSS design implementations

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import flask 
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+#load data from json file
+def index():
+    try:
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        data = []
+
+    return render_template('index.html', posts=data)
+
+@app.route('/save')
+def save_data():
+    data = [
+        {'id': 1, 'author': "John Doe", 'title': "First Post", 'content': "This is my first post"},
+        {'id': 2, 'author': "Jane Doe", 'title': "Second Post", 'content': "This is my second post"},
+    ]
+
+    try:
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+    except Exception as e:
+        print(f"Error writing file: {e}")
+        return "Error saving data"
+        
 
 if __name__ == '__main__':
     app.run()
