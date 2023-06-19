058cf9a369bdd7143d21c9aad7181101d263e65a
Evan <116227653+itsevanb@users.noreply.github.com>
2023-06-19 16:30:35 -0500

    adding functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -28,7 +28,36 @@ def save_data():
     except Exception as e:
         print(f"Error writing file: {e}")
         return "Error saving data"
-        
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        #get data from form
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        #save data to json file then load data to index page
+        try:
+            with open('data.json', 'r') as f:
+                data = json.load(f)
+        except Exception as e:
+            print(f"Error reading file: {e}")
+            data = []
+
+        new_data = {'id': len(data) + 1, 'author': author, 'title': title, 'content': content}
+        data.append(new_data)
+        try:
+            with open('data.json', 'w') as f:
+                json.dump(data, f)
+        except Exception as e:
+            print(f"Error writing file: {e}")
+            return "Error saving data"
+
+        return redirect(url_for('index'))
+    
+    
+    return render_template('add.html')
+
 
 if __name__ == '__main__':
     app.run()
